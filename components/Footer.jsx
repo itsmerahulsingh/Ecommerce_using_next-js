e382e0dd9199f2980187570d13ddeb01219be5d3
“itsmerahulsingh” <hotrahul5275@gmail.com>
2024-07-26 16:11:59 +0530

    first commit

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
-import { AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai';
+import React from "react";
+import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
 
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p>2022 JSM Headphones All rights reserverd</p>
+      <p>Developed by Sristy Singh</p>
+      <p>2024@All rights reserverd</p>
       <p className="icons">
-        <AiFillInstagram />
+        <AiFillInstagram/>
         <AiOutlineTwitter />
       </p>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
